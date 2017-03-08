750faa67ee126ba68dd59f5996e7bdd2ff9510b2
stoutfiles <shawn.stout@knetik.com>
2017-03-08 12:55:56 -0500

    JSAPI Groovy API update

---
+++ src/main/groovy/io/swagger/model/BareChallengeActivityResource.groovy
@@ -0,0 +1,20 @@
+package io.swagger.model;
+
+import groovy.transform.Canonical
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+@Canonical
+class BareChallengeActivityResource {
+
+  /* The id of the activity */
+  Long activityId = null
+
+  /* The id of the challenge */
+  Long challengeId = null
+
+  /* The unique ID for this resource */
+  Long id = null
+  
+
+}
+
