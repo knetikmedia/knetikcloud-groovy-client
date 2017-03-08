68db911a27fd06b92c3da6750a3e71ab8e282b6f
stoutfiles <shawn.stout@knetik.com>
2017-03-08 10:31:37 -0500

    JSAPI Groovy API update

---
+++ src/main/groovy/io/swagger/model/AggregateInvoiceReportResource.groovy
@@ -0,0 +1,19 @@
+package io.swagger.model;
+
+import groovy.transform.Canonical
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+@Canonical
+class AggregateInvoiceReportResource {
+
+    Long count = null
+
+    String date = null
+
+    Double revenue = null
+
+    Long userCount = null
+  
+
+}
+
